5db9091808f668f8b54f220678d8a1eeee2e00db
Geometry Tryhard <memelite69@gmail.com>
2025-07-25 15:51:59 -0400

    Update title.js

--- cloak/js/title.js
+++ cloak/js/title.js
@@ -1,14 +1,6 @@
-  const iframe = document.getElementById('framed');
-
-  const observer = new MutationObserver((mutationsList) => {
-    for (const mutation of mutationsList) {
-      if (mutation.type === 'attributes' && mutation.attributeName === 'src') {
-        alert(`Iframe src changed to: ${iframe.src}`);
-      }
-    }
-  });
-
-  observer.observe(iframe, {
-    attributes: true,
-    attributeFilter: ['src']
-  });
+function setTitle(){
+var src=document.getElementById('framed').src;
+  document.title=document.getElementById('framed').src;
+  setTimeout(setTitle, 250);
+};
+setTitle();
