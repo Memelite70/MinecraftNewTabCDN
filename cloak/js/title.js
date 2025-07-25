4167b9eedfb01480a73b0e1c838a1683110d5ae0
Geometry Tryhard <memelite69@gmail.com>
2025-07-25 15:11:10 -0400

    Update title.js

--- cloak/js/title.js
+++ cloak/js/title.js
@@ -1,6 +1,14 @@
-function setTitle(){
-var src=document.getElementById('framed').src;
-  document.title=document.getElementById('framed').contentDocument.title;
-  setTimeout(setTitle, 250);
-};
-setTitle();
+  const iframe = document.getElementById('framed');
+
+  const observer = new MutationObserver((mutationsList) => {
+    for (const mutation of mutationsList) {
+      if (mutation.type === 'attributes' && mutation.attributeName === 'src') {
+        alert(`Iframe src changed to: ${iframe.src}`);
+      }
+    }
+  });
+
+  observer.observe(iframe, {
+    attributes: true,
+    attributeFilter: ['src']
+  });
